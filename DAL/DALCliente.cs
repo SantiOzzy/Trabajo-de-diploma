2f69755a50851e288b52d98c1fc1743353355098
SantiOzzy <SantiOzzySheena@gmail.com>
2024-11-01 12:29:08 -0300

    Encapsulamiento reporte al realizar venta

--- DAL/DALCliente.cs
+++ DAL/DALCliente.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
+using BE;
 
 namespace DAL
 {
--- Trabajo de campo/FRMGenerarFactura.cs
+++ Trabajo de campo/FRMGenerarFactura.cs
@@ -150,115 +150,21 @@ namespace Trabajo_de_campo
 
         void GenerarReportePDF()
         {
-            SaveFileDialog savefile = new SaveFileDialog();
-            savefile.FileName = string.Format("{0}.pdf", DateTime.Now.ToString("ddMMyyyyHHmmss"));
+            FRMUI parent = this.MdiParent as FRMUI;
 
-            string PaginaHTML_Texto = Properties.Resources.Plantilla.ToString();
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FECHA", DateTime.Now.ToString("dd/MM/yyyy"));
+            DataTable ProductosVenta = negocios.ObtenerTabla("Libro.ISBN, Nombre, Autor, Precio, Cantidad", "Libro INNER JOIN Item ON Libro.ISBN = Item.ISBN", $"CodFactura = {NegociosFactura.ObtenerCodFactura()}");
+            string codFact = NegociosFactura.ObtenerCodFactura().ToString();
+            Cobro c = new Cobro(parent.fact.Cobro.MetodoPago, parent.fact.Cobro.Banco, parent.fact.Cobro.MarcaTarjeta, parent.fact.Cobro.TipoTarjeta);
+            DataTable dt = negocios.ObtenerTabla("DNI, Nombre, Apellido, Direccion, Email, NumTelefono", "Cliente", $"DNI = {parent.fact.DNI}");
+            Cliente cl = new Cliente(Convert.ToInt32(dt.Rows[0][0]), dt.Rows[0][1].ToString() + ' ' + dt.Rows[0][2].ToString(), dt.Rows[0][2].ToString(), Encriptacion.DesencriptarAES256(dt.Rows[0][3].ToString()), dt.Rows[0][4].ToString(), dt.Rows[0][5].ToString());
 
-            //DATOS DE PRODUCTOS
+            ReportesPDF.ReporteVenta(ProductosVenta, codFact, c, cl);
 
-            string filas = string.Empty;
-            decimal total = 0;
+            NegociosEvento.RegistrarEvento(new Evento(SessionManager.ObtenerInstancia().ObtenerDatosUsuario().Username, DateTime.Now.ToString("yyyy-MM-dd"), DateTime.Now.ToString("HH:mm:ss"), "Ventas", "Generación de reporte de factura de venta", 5));
 
-            DataTable dt = negocios.ObtenerTabla("Libro.ISBN, Nombre, Autor, Precio, Cantidad", "Libro INNER JOIN Item ON Libro.ISBN = Item.ISBN", $"CodFactura = {NegociosFactura.ObtenerCodFactura()}");
+            parent.FormBitacoraEventos.Actualizar();
 
-            foreach (DataRow r in dt.Rows)
-            {
-                filas += "<tr>";
-                filas += "<td>" + r[0].ToString() + "</td>";
-                filas += "<td>" + r[1].ToString() + "</td>";
-                filas += "<td>" + r[2].ToString() + "</td>";
-                filas += "<td>" + r[3].ToString() + "</td>";
-                filas += "<td>" + r[4].ToString() + "</td>";
-                filas += "</tr>";
-                total += (decimal.Parse(r[3].ToString()) * decimal.Parse(r[4].ToString()));
-            }
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FILAS1", filas);
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@TOTAL", total.ToString());
-
-            //DATOS DE VENTA
-
-            filas = string.Empty;
-
-            filas += "<tr>";
-            filas += "<td>" + NegociosFactura.ObtenerCodFactura() + "</td>";
-            filas += "<td>" + parent.fact.Cobro.MetodoPago + "</td>";
-            filas += "<td>" + parent.fact.Cobro.Banco + "</td>";
-            filas += "<td>" + parent.fact.Cobro.MarcaTarjeta + "</td>";
-            filas += "<td>" + parent.fact.Cobro.TipoTarjeta + "</td>";
-            filas += "</tr>";
-
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FILAS2", filas);
-
-            //DATOS DE CLIENTE
-
-            dt = negocios.ObtenerTabla("DNI, Nombre, Direccion, Email, NumTelefono", "Cliente", $"DNI = {parent.fact.DNI}");
-
-            filas = string.Empty;
-
-            filas += "<tr>";
-            filas += "<td>" + dt.Rows[0][0].ToString() + "</td>";
-            filas += "<td>" + dt.Rows[0][1].ToString() + "</td>";
-            filas += "<td>" + Encriptacion.DesencriptarAES256(dt.Rows[0][2].ToString()) + "</td>";
-            filas += "<td>" + dt.Rows[0][3].ToString() + "</td>";
-            filas += "<td>" + dt.Rows[0][4].ToString() + "</td>";
-            filas += "</tr>";
-
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FILAS3", filas);
-
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@REPORTEVENTA", LanguageManager.ObtenerInstancia().ObtenerTexto("Reporte.ReporteVenta"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@IMPRESIONFECHA", LanguageManager.ObtenerInstancia().ObtenerTexto("Reporte.FechaImpresion"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@PRECIOTOTAL", LanguageManager.ObtenerInstancia().ObtenerTexto("Reporte.Total"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@ISBN", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.ISBN"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@LIBRO", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.Nombre"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@AUTOR", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.Autor"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@PRECIO", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.Precio"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@CANTIDAD", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.Cantidad"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@CODFACTURA", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.CodFactura"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@METODOPAGO", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.MetodoPago"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@BANCO", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.Banco"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@MARCATARJETA", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.MarcaTarjeta"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@TIPOTARJETA", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.TipoTarjeta"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@DNI", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.DNI"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@NOMBRECLIENTE", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.NombreCliente"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@DIRECCION", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.Direccion"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@EMAIL", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.Email"));
-            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@NUMTEL", LanguageManager.ObtenerInstancia().ObtenerTexto("dgv.NumTelefono"));
-
-            if (savefile.ShowDialog() == DialogResult.OK)
-            {
-                using (FileStream stream = new FileStream(savefile.FileName, FileMode.Create))
-                {
-                    Document pdfDoc = new Document(PageSize.A4, 25, 25, 25, 25);
-
-                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
-                    pdfDoc.Open();
-                    pdfDoc.Add(new Phrase(LanguageManager.ObtenerInstancia().ObtenerTexto("Reporte.ReporteFactura")));
-
-                    iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(Properties.Resources.uai, System.Drawing.Imaging.ImageFormat.Png);
-                    img.ScaleToFit(150, 150);
-                    img.Alignment = iTextSharp.text.Image.UNDERLYING;
-
-                    img.SetAbsolutePosition(pdfDoc.Right - 150, pdfDoc.Top - 60);
-                    pdfDoc.Add(img);
-
-
-                    using (StringReader sr = new StringReader(PaginaHTML_Texto))
-                    {
-                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
-                    }
-
-                    pdfDoc.Close();
-                    stream.Close();
-                }
-
-                NegociosEvento.RegistrarEvento(new Evento(SessionManager.ObtenerInstancia().ObtenerDatosUsuario().Username, DateTime.Now.ToString("yyyy-MM-dd"), DateTime.Now.ToString("HH:mm:ss"), "Ventas", "Generación de reporte de factura de venta", 5));
-                FRMUI parent = this.MdiParent as FRMUI;
-                parent.FormBitacoraEventos.Actualizar();
-
-                MessageBox.Show(LanguageManager.ObtenerInstancia().ObtenerTexto("FRMGenerarFactura.Etiquetas.ReporteGenerado"));
-            }
+            MessageBox.Show(LanguageManager.ObtenerInstancia().ObtenerTexto("FRMGenerarReporteFactura.Etiquetas.ReporteGenerado"));
         }
     }
 }
